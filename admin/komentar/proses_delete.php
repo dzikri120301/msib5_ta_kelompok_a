680d218f88537d38ac15e9d922dbfbce03c90c59
Raisa <raisanurlita08@gmail.com>
2023-11-23 17:23:44 +0700

    Tugas Akhir-Tambah Komentar

--- admin/komentar/proses_delete.php
+++ admin/komentar/proses_delete.php
@@ -4,4 +4,4 @@ include 'koneksi.php';
 $result = mysqli_query($conn, "DELETE from genre where `id_nama_review` = '$_GET[id_nama_review]'");
 
 
-header("Location:index.php");
+header("Location:komentar.php");
