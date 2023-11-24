38b992b51417a7158362b9472a483218862bbc7c
Arydwntr <newbieprogramer201@gmail.com>
2023-11-25 00:16:18 +0700

    admin-ta

--- admin/admincineverse/tambah.php
+++ admin/admincineverse/tambah.php
@@ -5,37 +5,280 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tambah</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
+
+    <link href="../assets/img/favicon.png" rel="icon">
+    <link href="../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="../assets/vendor/quill/quill.snow.css" rel="stylesheet">
+    <link href="../assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+    <link href="../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="../assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css">
+
+    <!-- Template Main CSS File -->
+    <link href="../assets/css/style.css" rel="stylesheet">
+
+
+    <!-- =======================================================
+  * Template Name: NiceAdmin
+  * Updated: Nov 17 2023 with Bootstrap v5.3.2
+  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
 </head>
 
 <body>
     <?php
     include '../../config/koneksi.php';
     ?>
+    <header id="header" class="header fixed-top d-flex align-items-center">
 
-    <div class="container" style="max-width: 700px; margin-top:150px">
+<div class="d-flex align-items-center justify-content-between">
+    <a href="index.html" class="logo d-flex align-items-center">
+        <img src="../assets/img/logo.png" alt="">
+        <span class="d-none d-lg-block">Cineverse</span>
+    </a>
+    <i class="bi bi-list toggle-sidebar-btn"></i>
+</div><!-- End Logo -->
 
-        <form action=" proses_tambah.php" method="post" enctype="multipart/form-data" name="formtambah">
-            <div class="card mt-5">
-                <div class=" card-header text-center">
-                    <h3>ADD PAGE</h3>
-                </div>
-                <div class="card-body">
-                    <h6 class="card-title">Data ADMIN</h6>
+<div class="search-bar">
+    <form class="search-form d-flex align-items-center" method="POST" action="#">
+        <input type="text" name="query" placeholder="Search" title="Enter search keyword">
+        <button type="submit" title="Search"><i class="bi bi-search"></i></button>
+    </form>
+</div><!-- End Search Bar -->
+
+<nav class="header-nav ms-auto">
+    <ul class="d-flex align-items-center">
+
+        <li class="nav-item d-block d-lg-none">
+            <a class="nav-link nav-icon search-bar-toggle " href="#">
+                <i class="bi bi-search"></i>
+            </a>
+        </li><!-- End Search Icon-->
+
+        <li class="nav-item dropdown pe-3">
+
+            <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+                <img src="../assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
+                <span class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+            </a><!-- End Profile Iamge Icon -->
+
+            <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+                <li class="dropdown-header">
+                    <h6>Kevin Anderson</h6>
+                    <span>Web Designer</span>
+                </li>
+                <li>
+                    <hr class="dropdown-divider">
+                </li>
+
+                <li>
+                    <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+                        <i class="bi bi-person"></i>
+                        <span>My Profile</span>
+                    </a>
+                </li>
+                <li>
+                    <a class="dropdown-item d-flex align-items-center" href="#">
+                        <i class="bi bi-box-arrow-right"></i>
+                        <span>Sign Out</span>
+                    </a>
+                </li>
+
+            </ul><!-- End Profile Dropdown Items -->
+        </li><!-- End Profile Nav -->
+
+    </ul>
+</nav><!-- End Icons Navigation -->
+
+</header><!-- End Header -->
+
+<!-- ======= Sidebar ======= -->
+<aside id="sidebar" class="sidebar">
+<ul class="sidebar-nav" id="sidebar-nav">
+    <li class="nav-item">
+        <a class="nav-link " href="../dashboard.php">
+            <i class="bi bi-grid"></i>
+            <span>Dashboard</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="dashboard.php">
+            <i class="bi bi-menu-button-wide"></i><span>Film</span><i class="bi bi-chevron-down ms-auto"></i>
+        </a>
+        <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+            <li>
+                <a href="film.php">
+                    <i class="bi bi-circle"></i><span>Data film</span>
+                </a>
+            </li>
+            <li>
+                <a href="../genre/genre.php">
+                    <i class="bi bi-circle"></i><span>Genre Film</span>
+                </a>
+            </li>
+            <li>
+                <a href="../pemain/pemain.php">
+                    <i class="bi bi-circle"></i><span>Daftar Pemain</span>
+                </a>
+            </li>
+        </ul>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="dashboard.php">
+            <i class="bi bi-menu-button-wide"></i><span>User</span><i class="bi bi-chevron-down ms-auto"></i>
+        </a>
+        <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+            <li>
+                <a href="../user/user.php">
+                    <i class="bi bi-circle"></i><span>User Cineverse</span>
+                </a>
+            </li>
+            <li>
+                <a href="../komentar/komentar.php">
+                    <i class="bi bi-circle"></i><span>Review</span>
+                </a>
+            </li>
+        </ul>
+    </li><!-- End Forms Nav -->
+    <li class="nav-item">
+        <a class="nav-link" href="#">
+            <i class="bi bi-grid"></i>
+            <span>Admin Cineverse</span>
+        </a>
+    </li>
+</ul>
+</aside><!-- End Sidebar-->
+<main id="main" class="main">
+
+<div class="pagetitle">
+    <h1>Cineverse Add</h1>
+    <nav>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="../dashboard.php">Dashboard</a></li>
+            <li class="breadcrumb-item"><a href="film.php">Data Film</a></li>
+            <li class="breadcrumb-item active"><a href="tambah_film.php">Add Film</a></li>
+        </ol>
+    </nav>
+</div>
+
+<section class="section dashboard">
+            <div class="container-fluid">
+                <center>
+                    <div class="card text-bg-light mb-3" style="max-width: 50rem;">
+                        <div class="card-header">
+                            <h3>ADD PAGE</h3>
+                        </div>
+                            
+                    <div class="card-body">
+                    <h5 class="card-title">Data ADMIN</h5>
+                    <form action=" proses_tambah.php" method="post" enctype="multipart/form-data" name="formtambah">
                     <input class="form-control form-control-sm" type="text" onkeyup="checkform()" name="username" placeholder="Username" aria-label=".form-control-sm example"><br>
                     <input class="form-control form-control-sm" type="text" onkeyup="checkform()" name="password" placeholder="Password" aria-label=".form-control-sm example"><br>
                     <button type="submit" name="Submit" value="Simpan" class="btn btn-success">Submit</button>
-                </div>
-
-                </tbody>
-                </table>
+                    </div>
+                    </form>
+                    </div>
             </div>
-        </form>
+                </center>
+            </div>
+        </section>
 
+        </main><!-- End #main -->
 
+<!-- ======= Footer ======= -->
+<footer id="footer" class="footer">
+    <div class="copyright">
+        &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
     </div>
-</body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+    </div>
+</footer><!-- End Footer -->
 
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="../assets/vendor/apexcharts/apexcharts.min.js"></script>
+<script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../assets/vendor/chart.js/chart.umd.js"></script>
+<script src="../assets/vendor/echarts/echarts.min.js"></script>
+<script src="../assets/vendor/quill/quill.min.js"></script>
+<script src="../assets/vendor/simple-datatables/simple-datatables.js"></script>
+<script src="../assets/vendor/tinymce/tinymce.min.js"></script>
+<script src="../assets/vendor/php-email-form/validate.js"></script>
+<!-- Template Main JS File -->
+<script src="../assets/js/main.js"></script>
+
+<script src="https://kit.fontawesome.com/6beb2a82fc.js" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+<script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
+<!-- Add this script block at the end of your <body> section -->
+<!-- Add this script block at the end of your <body> section -->
+<!-- Add this script block at the end of your <body> section -->
+<!-- <script>
+    $(document).ready(function() {
+        $("#kode_kamar").on("blur", function() {
+            var kode_kamar = $(this).val();
+
+            $.ajax({
+                url: "check_kode_kamar.php",
+                type: "POST",
+                data: {
+                    kode_kamar: kode_kamar
+                },
+                success: function(response) {
+                    if (response == "exists") {
+                        alert("Kode Kamar yang anda masukkan sudah ada");
+                        $("#kode_kamar").val(""); // Clear the input field
+                    }
+                }
+            });
+        });
+
+        function checkInputs() {
+            var allInputsFilled = true;
+
+            // Loop through each input field
+            $('form#tambah_kamar input').each(function() {
+                // Check if the input field is empty
+                if ($(this).val() === '') {
+                    allInputsFilled = false;
+                }
+            });
+
+            // Enable or disable the submit button based on the result
+            $('#submitBtn').prop('disabled', !allInputsFilled);
+        }
+
+        // Add event listener for input fields
+        $('form#tambah_kamar input').on('input', function() {
+            checkInputs();
+        });
+
+        // Add event listener for the select fields
+        $('form#tambah_kamar select').on('change', function() {
+            checkInputs();
+        });
+    });
+</script> -->
+</body>
 </html>
