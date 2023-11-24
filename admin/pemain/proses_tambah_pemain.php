3b5f4880b47c0f919fcf13bcc64178929ba7347e
Raisa <raisanurlita08@gmail.com>
2023-11-24 13:46:00 +0700

    Tugas Akhir - Pemain

--- admin/pemain/proses_tambah_pemain.php
+++ admin/pemain/proses_tambah_pemain.php
@@ -1,21 +1,20 @@
 <?php
-    $nama_pemain = $_POST['nama_pemain'];
-    $file_gambar = $_FILES['foto']['name'];
-    $asal = $_POST['asal'];
-    $tanggal_lahir = $_POST['tgl_lahir'];
+$nama_pemain = $_POST['nama_pemain'];
+$file_gambar = $_FILES['foto']['name'];
+$asal = $_POST['asal'];
+$tanggal_lahir = $_POST['tgl_lahir'];
 
-    include('../../config/koneksi.php');
+include('../../config/koneksi.php');
 
-    $file_gambar = $_FILES['foto']['name'];
-    $tmp_file = $_FILES['foto']['tmp_name'];
+$file_gambar = $_FILES['foto']['name'];
+$tmp_file = $_FILES['foto']['tmp_name'];
 
-    $path = "images";
-    $target_file = $path . '/' . basename($file_gambar);
-    move_uploaded_file($tmp_file, $target_file);
+$path = "images";
+$target_file = $path . '/' . basename($file_gambar);
+move_uploaded_file($tmp_file, $target_file);
 
 
 
-    $result = mysqli_query($conn, "INSERT INTO `pemain` (`nama_pemain`, `foto`, `asal`, `tgl_lahir`) VALUES ('$nama_pemain', '$file_gambar', '$asal', '$tanggal_lahir');");
+$result = mysqli_query($conn, "INSERT INTO `pemain` (`nama_pemain`, `foto`, `asal`, `tgl_lahir`) VALUES ('$nama_pemain', '$file_gambar', '$asal', '$tanggal_lahir');");
 
-    header("Location: pemain.php");
-?>
+header("Location: pemain.php");
