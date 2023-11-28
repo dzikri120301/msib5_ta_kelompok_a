da34979057b0b1e9ec641e4fb67b6ffe2f1d2477
dzikri120301 <muhammaddzikrimufarrij.com>
2023-11-28 20:11:43 +0700

    commit terbaru

--- Cineverse/About.php
+++ Cineverse/About.php
@@ -34,8 +34,12 @@
     </style>
 
 </head>
-
 <body>
+    <?php
+        include '../config/koneksi.php';
+        $query = mysqli_query($conn, "SELECT * FROM saran ORDER BY id_saran Asc");
+    ?>
+    
     <!-- ======= Header ======= -->
     <header id="header" class="fixed-top">
         <div class="container d-flex align-items-center">
@@ -345,12 +349,13 @@
 
                 <div class="row">
 
+
                     <div class="col-lg-12 mt-5 mt-lg-0 d-flex align-items-center">
-                        <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+                        <form action="proses_tambah_saran.php" method="post" role="form" class="php-email-form" id="contact">
                             <div class="row">
                                 <div class="form-group">
                                     <label for="name">Username</label>
-                                    <input type="text" name="name" class="form-control" id="name" required />
+                                    <input type="text" name="username" class="form-control" id="username" required />
                                 </div>
                             </div>
                             <div class="form-group">
@@ -359,7 +364,7 @@
                             </div>
                             <div class="form-group">
                                 <label for="name">Message</label>
-                                <textarea class="form-control" name="message" rows="10" required></textarea>
+                                <textarea class="form-control" name="pesan" rows="10" id="pesan" required></textarea>
                             </div>
                             <div class="my-3">
                                 <div class="loading">Loading</div>
---
+++ admin/kotaksaran/proses_tambah_saran.php
@@ -0,0 +1,11 @@
+<?php
+include '../config/koneksi.php';
+
+$username = $_POST["username"];
+$subject = $_POST["subject"];
+$pesan = $_POST["subject"];
+
+$result = mysqli_query($conn, "INSERT INTO `saran` (`id_saran`,`username`,`subject`,`pesan`) VALUES ('','$username','$subject','$pesan');");
+
+
+header("Location:About.php");
