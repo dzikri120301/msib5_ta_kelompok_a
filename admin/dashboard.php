304da17ec6c89abb6d89ecb9d87081a17d382de6
Raisa <raisanurlita08@gmail.com>
2023-11-29 17:14:46 +0700

    Dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -166,109 +166,123 @@ if (!isset($_SESSION["username"])) {
         <section class="section dashboard">
             <div class="row">
                 <!-- Left side columns -->
-                <div class="col-lg-8">
+                <div class="col-lg-10">
                     <div class="row">
-                        <!-- Sales Card -->
+                        <!-- User Card -->
                         <div class="col-xxl-4 col-md-6">
                             <div class="card info-card sales-card">
 
-                                <div class="filter">
-                                    <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                                        <li class="dropdown-header text-start">
-                                            <h6>Filter</h6>
-                                        </li>
-
-                                        <li><a class="dropdown-item" href="#">Today</a></li>
-                                        <li><a class="dropdown-item" href="#">This Month</a></li>
-                                        <li><a class="dropdown-item" href="#">This Year</a></li>
-                                    </ul>
-                                </div>
 
                                 <div class="card-body">
-                                    <h5 class="card-title">Sales <span>| Today</span></h5>
-
+                                    <?php
+                                    include '../config/koneksi.php';
+                                    $query = mysqli_query($conn, "SELECT COUNT(*) AS total FROM `user`");
+                                    // Mengambil hasil query
+                                    $result = mysqli_fetch_assoc($query);
+                                    // Mengambil nilai total
+                                    $total = $result['total'];
+                                    // Optional: Tutup koneksi ke database jika sudah tidak digunakan
+                                    mysqli_close($conn);
+                                    ?>
+                                    <h5 class="card-title">User<span> | Total</span></h5>
                                     <div class="d-flex align-items-center">
                                         <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                                            <i class="bi bi-cart"></i>
+                                            <a href="user/user.php" style="color: #4154f1"><i class="bi bi-people"></i></a>
                                         </div>
                                         <div class="ps-3">
-                                            <h6>145</h6>
-                                            <span class="text-success small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">increase</span>
-
+                                            <h6><?php echo "" . $total; ?></h6>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div><!-- End Sales Card -->
+                        </div><!-- End User Card -->
 
-                        <!-- Revenue Card -->
+                        <!-- Film Card -->
+                        <div class="col-xxl-4 col-md-6">
+                            <div class="card info-card customers-card">
+                                <div class="card-body">
+                                    <?php
+                                    include '../config/koneksi.php';
+                                    $query = mysqli_query($conn, "SELECT COUNT(*) AS total FROM `tb_film`");
+                                    // Mengambil hasil query
+                                    $result = mysqli_fetch_assoc($query);
+                                    // Mengambil nilai total
+                                    $total = $result['total'];
+                                    // Optional: Tutup koneksi ke database jika sudah tidak digunakan
+                                    mysqli_close($conn);
+                                    ?>
+                                    <h5 class="card-title">Film<span> | Total</span></h5>
+                                    <div class="d-flex align-items-center">
+                                        <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                            <a href="film/film.php" style="color: #ff771d"><i class=" bi bi-collection-play"></i></a>
+                                        </div>
+                                        <div class="ps-3">
+                                            <h6><?php echo "" . $total; ?></h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!-- End Film Card -->
+
+                        <!-- Genre Card -->
                         <div class="col-xxl-4 col-md-6">
                             <div class="card info-card revenue-card">
-                                <div class="filter">
-                                    <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                                        <li class="dropdown-header text-start">
-                                            <h6>Filter</h6>
-                                        </li>
-                                        <li><a class="dropdown-item" href="#">Today</a></li>
-                                        <li><a class="dropdown-item" href="#">This Month</a></li>
-                                        <li><a class="dropdown-item" href="#">This Year</a></li>
-                                    </ul>
-                                </div>
-
                                 <div class="card-body">
-                                    <h5 class="card-title">Revenue <span>| This Month</span></h5>
+                                    <?php
+                                    include '../config/koneksi.php';
+                                    $query = mysqli_query($conn, "SELECT COUNT(*) AS total FROM `genre`");
+                                    // Mengambil hasil query
+                                    $result = mysqli_fetch_assoc($query);
+                                    // Mengambil nilai total
+                                    $total = $result['total'];
+                                    // Optional: Tutup koneksi ke database jika sudah tidak digunakan
+                                    mysqli_close($conn);
+                                    ?>
+                                    <h5 class="card-title">Genre<span> | Total</span></h5>
                                     <div class="d-flex align-items-center">
                                         <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                                            <i class="bi bi-currency-dollar"></i>
+                                            <a href="genre/genre.php" style="color: #7B5A84"><i class="bi bi-list-task"></i></a>
                                         </div>
                                         <div class="ps-3">
-                                            <h6>$3,264</h6>
-                                            <span class="text-success small pt-1 fw-bold">8%</span> <span class="text-muted small pt-2 ps-1">increase</span>
-
+                                            <h6><?php echo "" . $total; ?></h6>
                                         </div>
                                     </div>
                                 </div>
 
                             </div>
-                        </div><!-- End Revenue Card -->
+                        </div><!-- End Genre Card -->
 
-                        <!-- Customers Card -->
-                        <div class="col-xxl-4 col-xl-12">
-                            <div class="card info-card customers-card">
-                                <div class="filter">
-                                    <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                                        <li class="dropdown-header text-start">
-                                            <h6>Filter</h6>
-                                        </li>
-                                        <li><a class="dropdown-item" href="#">Today</a></li>
-                                        <li><a class="dropdown-item" href="#">This Month</a></li>
-                                        <li><a class="dropdown-item" href="#">This Year</a></li>
-                                    </ul>
-                                </div>
-
+                        <!-- Review Card -->
+                        <div class="col-xxl-4 col-md-6">
+                            <div class="card info-card review-card">
                                 <div class="card-body">
-                                    <h5 class="card-title">Customers <span>| This Year</span></h5>
+                                    <?php
+                                    include '../config/koneksi.php';
+                                    $query = mysqli_query($conn, "SELECT COUNT(*) AS total FROM `komentar`");
+                                    // Mengambil hasil query
+                                    $result = mysqli_fetch_assoc($query);
+                                    // Mengambil nilai total
+                                    $total = $result['total'];
+                                    // Optional: Tutup koneksi ke database jika sudah tidak digunakan
+                                    mysqli_close($conn);
+                                    ?>
+                                    <h5 class="card-title">Review<span> | Total</span></h5>
                                     <div class="d-flex align-items-center">
                                         <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                                            <i class="bi bi-people"></i>
+                                            <a href="komentar/komentar.php" style="color: #409261"> <i class="bi bi-chat-left-text"></i></a>
                                         </div>
                                         <div class="ps-3">
-                                            <h6>1244</h6>
-                                            <span class="text-danger small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">decrease</span>
-
+                                            <h6><?php echo "" . $total; ?></h6>
                                         </div>
                                     </div>
                                 </div>
+
                             </div>
-                        </div><!-- End Customers Card -->
+                        </div><!-- End Review Card -->
                     </div>
                 </div><!-- End Left side columns -->
             </div>
         </section>
-
     </main><!-- End #main -->
 
     <!-- ======= Footer ======= -->
